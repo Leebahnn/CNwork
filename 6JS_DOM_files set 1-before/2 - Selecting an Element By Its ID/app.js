ffb39688f85e41620bb9339ff4ade4dfffb0c021
Leebahnn <amarismaagil@gmail.com>
2019-02-15 13:07:53 +0000

    cool

---
+++ 6JS_DOM_files set 1-before/2 - Selecting an Element By Its ID/app.js
@@ -0,0 +1,9 @@
+const heading = document.getElementById("heading"); 
+
+const input = document.getElementById("input");
+
+const button = document.getElementByI("button");
+
+button.addEventListener("click", () => {
+    heading.style.color = input.value;
+   });  
